29e022f68dd4aad8cfecfb9585f7e83dcb62e349
Ivan Petrov <lazytechwork@gmail.com>
2022-12-03 19:59:20 +0300

    Removed unnecessary files

--- php8.0-ubuntu.sh
+++
@@ -1,2 +0,0 @@
-docker build -t lazytechwork/php:8.0 -f ./php/8.0/Dockerfile ./php/8.0/ &&
-  docker push lazytechwork/php:8.0
--- ubuntu-20.04.sh
+++
@@ -1,2 +0,0 @@
-docker build -t lazytechwork/ubuntu:20.04 -f ./ubuntu/20.04.Dockerfile ./ubuntu/ &&
-  docker push lazytechwork/ubuntu:20.04
