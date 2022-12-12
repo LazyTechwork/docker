dcb45bcbd3a8b79cebe4288db8a27a2d6d8295aa
Ivan Petrov <lazytechwork@gmail.com>
2022-12-12 18:12:13 +0300

    Улучшения и настройка XDebug

--- php/8.0-ubuntu/start.sh
+++ php/8.0-ubuntu/start.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 if [ ! -z "$APPUSER" ]; then
-    usermod -u 1337 $APPUSER
+  usermod -u 1337 "$APPUSER"
 fi
 
 if [ ! -d /.composer ]; then
-    mkdir /.composer
+  mkdir /.composer
 fi
 
+usermod -aG "$APPUSER" root
 chmod -R ugo+rw /.composer
-chown -R $APPUSER:$APPUSER /var/www/application
+chown -R "$APPUSER":"$APPUSER" /var/www/application
 
-if [ $# -gt 0 ];then
-    exec gosu $APPUSER "$@"
+if [ $# -gt 0 ]; then
+  exec gosu "$APPUSER" "$@"
 else
-    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+  /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 fi
